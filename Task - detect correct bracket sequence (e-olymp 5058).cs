77ee56e3f986a800da75c37c6e1a51080cfa6645
auto-no-mous <67557657+auto-no-mous@users.noreply.github.com>
2020-10-25 11:07:11 +0300

    Add files via upload

---
+++ Task - detect correct bracket sequence (e-olymp 5058).cs
@@ -0,0 +1,94 @@
+﻿//solution to https://www.e-olymp.com/ru/problems/5058
+//solved with using self-written stack, as example for data structures
+
+using System;
+using System.Globalization;
+
+public class Stack <T>
+{
+    private T[] items;
+    private int count;
+
+    public Stack (int length)
+    {
+        items = new T[length];
+        count = 0;
+    }
+
+    public bool IsEmpty
+    {
+        get
+        {
+            return count == 0;
+        }
+    }
+
+    public void Push(T item)
+    {
+        if (count == items.Length)
+            throw new InvalidOperationException("Стек полон, невозможно добавить элемент.");
+        items[count++] = item;
+    }
+
+    public T Pop()
+    {
+        if (IsEmpty)
+            throw new InvalidOperationException("Стек пуст, нечего извлекать.");
+        count--;
+        return items[count];
+    }
+}
+
+class Brackets
+{
+    static void Main(string[] args)
+    {
+        Stack<char> st = new Stack<char>(256);
+        string input = Console.ReadLine();
+        for (int i = 0; i < input.Length; i++)
+        {
+            if ((input[i] == '(') || (input[i] == '[') || (input[i] == '{'))
+                st.Push(input[i]);
+            else 
+            {
+                try
+                {
+                    switch (input[i])
+                    {
+                        case ')':
+                            if (st.Pop() != '(')
+                            {
+                                Console.WriteLine("no");
+                                return;
+                            }
+                            break;
+
+                        case ']':
+                            if (st.Pop() != '[')
+                            {
+                                Console.WriteLine("no");
+                                return;
+                            }
+                            break;
+
+                        case '}':
+                            if (st.Pop() != '{')
+                            {
+                                Console.WriteLine("no");
+                                return;
+                            }
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("no");
+                    return;
+                }
+            }          
+        }
+        if (st.IsEmpty)
+            Console.WriteLine("yes");
+        else Console.WriteLine("no");
+    }
+}
