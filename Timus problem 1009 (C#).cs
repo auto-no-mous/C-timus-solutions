25d4fbc34292a02c2a8f9ca5a414d1706c90f256
Aleksandr Kiselyov <67557657+auto-no-mous@users.noreply.github.com>
2021-02-22 16:36:02 +0300

    Update Timus problem 1009 (C#).cs

--- Timus problem 1009 (C#).cs
+++ Timus problem 1009 (C#).cs
@@ -23,7 +23,7 @@ namespace Test
             dp[1] = k - 1;
             for (int i = 2; i <= n; i++)
             {
-                dp[i] = dp[i - 1] * (k - 1) + dp[i-2] * (k-1);
+                dp[i] = dp[i - 1] * (k - 1) + dp[i - 2] * (k - 1);
             }
             return dp[n];
         }
