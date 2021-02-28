466820904306a6ec9caad4dc3e0c8f1fe68fd835
Aleksandr Kiselyov <67557657+auto-no-mous@users.noreply.github.com>
2021-02-28 20:12:54 +0300

    Add files via upload

---
+++ Timus problem 1023 (C#).cs
@@ -0,0 +1,26 @@
+﻿// https://acm.timus.ru/problem.aspx?space=1&num=1023
+using System;
+
+namespace timus1023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+            int divisor = k;
+            for (int i = 3; i <(k/2)+1; i++)
+            {
+                if (k % i == 0)
+                {
+                    divisor = i;
+                    break;
+                }
+            }
+            Console.WriteLine(divisor - 1);
+            
+        }
+    }
+
+
+}
