6b073b29597bb318e433725019177c1708268d23
Aleksandr Kiselyov <67557657+auto-no-mous@users.noreply.github.com>
2021-04-17 10:45:52 +0300

    Add files via upload

---
+++ Timus problem 1001 (C++).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <list>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{ 
+	long long n;
+	list<long long> arr;
+	while (scanf_s("%lld", &n) != EOF)
+	{
+		arr.push_front(n);
+	}
+
+	for (long long j: arr)
+		printf("%.4f\n", sqrt(j));
+	return 0;
+}
