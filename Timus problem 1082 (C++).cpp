fee2ca0959978b459589bd5e3f71dd824e85fcd3
Aleksandr Kiselyov <67557657+auto-no-mous@users.noreply.github.com>
2021-04-17 10:49:24 +0300

    Add files via upload

---
+++ Timus problem 1082 (C++).cpp
@@ -0,0 +1,14 @@
+﻿// https://acm.timus.ru/problem.aspx?space=1&num=1082
+// meaningless solution, but accepted
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cout << i << " ";
+    return 0;
+}
